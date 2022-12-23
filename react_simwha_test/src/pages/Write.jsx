0527fc9aa9ab98289c8781986baf6daa52a08f18
hodu_Mom <aldud7798555@gmail.com>
2022-12-23 20:59:33 +0900

    별점기능+게시글 저장기능 완료

--- react_simwha_test/src/pages/Write.jsx
+++ react_simwha_test/src/pages/Write.jsx
@@ -1,18 +1,82 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import ReactStars from 'react-rating-stars-component';
 import styled from 'styled-components';
 import Layout from '../components/Layout';
 import Editor from '../components/Editor';
+import {__addWriteThunk} from '../redux/modules/addupdateSlice';
+import {useDispatch, useSelector} from 'react-redux';
+import axios from 'axios';
+import { render } from 'react-dom';
+import ReactHtmlParser from 'react-html-parser';
+
 const Write = () => {
+  const dispatch = useDispatch();
+
+  
+  const [content, setContent] = useState('');
+  // console.log(content)
+
+  // handler
+  const onClickSubmitWriteHandler = (e) => {
+    e.preventDefault();
+
+    const newpost = {
+      user_id: 'miyoung',
+      id: Date.now(),
+      title,
+      content,
+      rate,
+    };
+
+    dispatch(__addWriteThunk(newpost));
+
+  };
+
+  // 제목
+  const [title, setTitle] = useState('');
+  const onChangeTitleHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+
+  // 별점 라이브러리
+  const [rate, setRate] = useState(0);
+  const ratingChanged = (newRating) => {
+    setRate(newRating);
+  };
+
+
+
   return (
     <Layout>
       <StyledP>✏️ 우리동네의 붕어빵 맛집을 알려주세요.</StyledP>
-      <WirteContainer>
-        <StyledInput placeholder='제목을 입력해주세요.' />
-        <Editor />
-      </WirteContainer>
-      <Flex>
-        <AddButton>등록</AddButton>
-      </Flex>
+      <form onSubmit={onClickSubmitWriteHandler}>
+        <WirteContainer>
+          <Flex
+            alingItems='center'
+            borderBottom='3px solid #f2d589'
+            width='250px'
+          >
+            <Star>만족도ㅤ</Star>
+            <ReactStars
+              size={30}
+              activeColor='#f2d589'
+              onChange={ratingChanged}
+            />
+          </Flex>
+
+          <StyledInput
+            placeholder='제목을 입력해주세요.'
+            value={title}
+            onChange={onChangeTitleHandler}
+          />
+          <Editor setContent={setContent} content={content} />
+          <Flex justifyCt='right' marginTop='60px'>
+            <AddButton>등록</AddButton>
+          </Flex>
+        </WirteContainer>
+      </form>
+      <div>{ReactHtmlParser(content)}</div>
     </Layout>
   );
 };
@@ -47,8 +111,11 @@ const StyledInput = styled.input`
 
 const Flex = styled.div`
   display: flex;
-  justify-content: right;
-  margin-top: 60px;
+  justify-content: ${(props) => props.justifyCt};
+  align-items: ${(props) => props.alingItems};
+  margin-top: ${(props) => props.marginTop};
+  border-bottom: ${(props) => props.borderBottom};
+  width: ${(props) => props.width};
 `;
 
 const AddButton = styled.button`
@@ -65,6 +132,11 @@ const AddButton = styled.button`
     width: 95px;
     height: 35px;
   }
+`;
+
+const Star = styled.span`
+  font-size: 20px;
+  display: inline;
 `;
 
 export default Write;
---
+++ react_simwha_test/src/redux/modules/addupdateSlice.js
@@ -0,0 +1,83 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios';
+
+
+export const __addWriteThunk = createAsyncThunk(
+  'addWirte', // action value
+  async (payload, thunkAPI) => { // 콜백함수
+    try {
+      const { data } = await axios.post(`http://localhost:3001/posts`,payload);
+      console.log(data)
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+// export const __updateTodoThunk = createAsyncThunk(
+//   'UPDATE_TODO',
+//   async (arg, thunkAPI) => {
+//     try {
+//       axios.patch(`http://localhost:3001/todos/${arg.id}`, arg);
+//       return thunkAPI.fulfillWithValue(arg);
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.code);
+//     }
+//   }
+// );
+
+const initialState = {
+  posts: {
+    user_id: 'miyoung',
+    id: Date.now(),
+    title: '',
+    content: '',
+    rate: '',
+  },
+  error: null, // 서버랑 통신 실패 시 나타내는 에러메세지 담아놓는 값
+  isLoading: false, // 서버에서 posts를 가져오는 상태값
+};
+
+export const addupdateSlice = createSlice({
+  name: 'addWirte',
+  initialState,
+  reducers: {
+    addPost: (state) => {
+      state.contents = {
+        user_id: 'miyoung',
+        id: Date.now(),
+        title: '',
+        content: '',
+        rate: '',
+      };
+    },
+  },
+  extraReducers: {
+    [__addWriteThunk.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.posts = action.payload;
+    },
+    [__addWriteThunk.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    [__addWriteThunk.pending]: (state) => {
+      state.isLoading = true;
+    },
+    // [__updateTodoThunk.fulfilled]: (state, action) => {
+    //   state.isLoading = false;
+    //   state.todo = action.payload;
+    // },
+    // [__updateTodoThunk.pending]: (state) => {
+    //   state.isLoading = true;
+    // },
+    // [__updateTodoThunk.rejected]: (state, action) => {
+    //   state.isLoading = false;
+    //   state.error = action.payload;
+    // },
+  },
+});
+
+export const {addPost} = addupdateSlice.actions;
+export default addupdateSlice.reducer;
