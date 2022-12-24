4ef5b4b46b9769e482319867c6a297f5ae032c75
hodu_Mom <aldud7798555@gmail.com>
2022-12-24 15:36:26 +0900

    홈 UI완성-미영-

--- react_simwha_test/src/pages/Home.jsx
+++ react_simwha_test/src/pages/Home.jsx
@@ -1,10 +1,182 @@
-import React from "react";
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
-import Layout from "../components/Layout";
+import React from 'react';
+import styled from 'styled-components';
+import {useNavigate} from 'react-router-dom';
+import Layout from '../components/Layout';
+import {Dropdown, DropdownButton} from 'react-bootstrap';
 
 const Home = () => {
-  return <Layout>main page</Layout>;
+  return (
+    <Layout>
+      <MainBox>
+        <SortBox>
+          <DropdownButton id='dropdown-item-button' title='---정렬---'>
+            <Dropdown.Item as='button'>전체</Dropdown.Item>
+            <Dropdown.Item as='button'>조회순</Dropdown.Item>
+            <Dropdown.Item as='button'>평점순</Dropdown.Item>
+          </DropdownButton>
+        </SortBox>
+
+        <ItemContainer>
+          <Items>
+            <Item>
+              <RatingBox>별점나오는곳</RatingBox>
+              <TitleBox>
+                타이틀타이틀타이틀타타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀이틀타이틀타이틀타이틀타이틀타이틀
+              </TitleBox>
+              <ContentBox>
+                내용내용내용내용내용내내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용용내용내용내용내용내용
+                내용 내용 내용 내용 내용 내용 내용
+                내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+              </ContentBox>
+              <UserNameBox>유저이름</UserNameBox>
+            </Item>
+            <Item>
+              <RatingBox>별점나오는곳</RatingBox>
+              <TitleBox>
+                타이틀타이틀타이틀타타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀이틀타이틀타이틀타이틀타이틀타이틀
+              </TitleBox>
+              <ContentBox>
+                내용내용내용내용내용내내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용용내용내용내용내용내용
+                내용 내용 내용 내용 내용 내용 내용
+                내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+              </ContentBox>
+              <UserNameBox>유저이름</UserNameBox>
+            </Item>
+            <Item>
+              <RatingBox>별점나오는곳</RatingBox>
+              <TitleBox>
+                타이틀타이틀타이틀타타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀이틀타이틀타이틀타이틀타이틀타이틀
+              </TitleBox>
+              <ContentBox>
+                내용내용내용내용내용내내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용용내용내용내용내용내용
+                내용 내용 내용 내용 내용 내용 내용
+                내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+              </ContentBox>
+              <UserNameBox>유저이름</UserNameBox>
+            </Item>
+            <Item>
+              <RatingBox>별점나오는곳</RatingBox>
+              <TitleBox>
+                타이틀타이틀타이틀타타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀이틀타이틀타이틀타이틀타이틀타이틀
+              </TitleBox>
+              <ContentBox>
+                내용내용내용내용내용내내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용용내용내용내용내용내용
+                내용 내용 내용 내용 내용 내용 내용
+                내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+              </ContentBox>
+              <UserNameBox>유저이름</UserNameBox>
+            </Item>
+            <Item>
+              <RatingBox>별점나오는곳</RatingBox>
+              <TitleBox>
+                타이틀타이틀타이틀타타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀타이틀이틀타이틀타이틀타이틀타이틀타이틀
+              </TitleBox>
+              <ContentBox>
+                내용내용내용내용내용내내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용용내용내용내용내용내용
+                내용 내용 내용 내용 내용 내용 내용
+                내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+              </ContentBox>
+              <UserNameBox>유저이름</UserNameBox>
+            </Item>
+          </Items>
+        </ItemContainer>
+      </MainBox>
+    </Layout>
+  );
 };
 
 export default Home;
+
+const MainBox = styled.main``;
+const SortBox = styled.section`
+  text-align: right;
+  border-top: 3px solid #f2d589;
+  padding-top: 10px;
+  box-sizing: inherit;
+  margin-bottom: 15px;
+`;
+const ItemContainer = styled.div`
+  margin: 0 auto;
+  box-sizing: inherit;
+`;
+const Items = styled.ul`
+  list-style: none;
+  margin: 0;
+  display: flex;
+  grid-gap: 35px;
+  gap: 35px;
+  flex-wrap: wrap;
+  padding: 0;
+  box-sizing: inherit;
+`;
+const Item = styled.a`
+  opacity: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 40px 26px 26px 34px;
+  width: 300px;
+  height: 370px;
+  background-color: #fff;
+  border: 2px solid #f2d589;
+  border-radius: 30px;
+  position: relative;
+  cursor: pointer;
+  transition: 0.2s;
+  box-sizing: inherit;
+  &:link {
+    text-decoration: none;
+    color: #f2d589;
+  }
+  &:visited {
+    text-decoration: none;
+    color: #f2d589;
+  }
+  &:active {
+    text-decoration: none;
+    color: #f2d589;
+  }
+  &:hover {
+    text-decoration: none;
+    color: #f2d589;
+    width: 310px;
+    height: 380px;
+  }
+`;
+
+const TitleBox = styled.h2`
+  font-size: 20px;
+  height: 76px;
+  line-height: 28px;
+  font-weight: 500;
+  letter-spacing: -0.04em;
+  margin: 16px 0;
+  word-break: break-all;
+  overflow: hidden;
+  box-sizing: inherit;
+`;
+const ContentBox = styled.p`
+  font-size: 16px;
+  height: 160px;
+  line-height: 24px;
+  font-weight: 400;
+  letter-spacing: -0.04em;
+  margin: 11px 0;
+  word-break: break-all;
+  overflow: hidden;
+  box-sizing: inherit;
+`;
+
+const UserNameBox = styled.div`
+  font-size: 18px;
+  border-top: 2px solid #f0e0b5;
+  padding-top: 16px;
+  color: #f2d589;
+  font-weight: 700;
+  box-sizing: inherit;
+`;
+
+const RatingBox = styled.div`
+  font-size: 20px;
+  box-sizing: inherit;
+`;
--- react_simwha_test/src/shared/GlobalStyle.jsx
+++ react_simwha_test/src/shared/GlobalStyle.jsx
@@ -1,13 +1,22 @@
-import { createGlobalStyle } from "styled-components";
+import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 html,
 body {
   padding: 0;
-  margin: 0;
+  margin: 0 auto;
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  max-width: 1400px;
+  min-width: 800px;
 }
 
+li {
+  padding: 0;
+}
+
+li, p {
+  margin: 0;
+}
 a {
   color: inherit;
   text-decoration: none;
@@ -16,6 +25,36 @@ a {
 textarea,
 pre {
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+.btn-primary {
+    --bs-btn-color: #fff;
+    --bs-btn-bg: #f2d589;
+    --bs-btn-border-color: #f2d589;
+    --bs-btn-hover-color: #fff;
+    --bs-btn-hover-bg: #f2d589;
+    --bs-btn-hover-border-color: none;
+    --bs-btn-focus-shadow-rgb: 49,132,253;
+    --bs-btn-active-color: #fff;
+    --bs-btn-active-bg: #e7ca82;
+    --bs-btn-active-border-color: none;
+    --bs-btn-active-shadow: inset 0 3px 5pxrgba(0, 0, 0, 0.125);
+    --bs-btn-disabled-color: #fff;
+    --bs-btn-disabled-bg: none;
+    --bs-btn-disabled-border-color: none;
+}
+
+.btn {
+  --bs-btn-font-weight: 700;
+}
+
+.dropdown-menu {
+  --bs-dropdown-link-hover-bg: #eee5ceb5;
+  --bs-dropdown-link-active-bg: #eee0b9;
+}
+
+.dropdown-item {
+  font-weight: 600;
 }
 
 * {
