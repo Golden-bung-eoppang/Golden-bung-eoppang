cadab731eeecce8e775a9a6ba8f540424e1f184f
hodu_Mom <aldud7798555@gmail.com>
2022-12-26 17:28:27 +0900

    도원님꺼 머지하려고 푸시함

--- react_simwha_test/src/redux/modules/addupdateSlice.js
+++ react_simwha_test/src/redux/modules/addupdateSlice.js
@@ -7,7 +7,6 @@ export const __addWriteThunk = createAsyncThunk(
     // 콜백함수
     try {
       const { data } = await axios.post(`http://localhost:3001/posts`, payload);
-      console.log("오류확인: ", data);
       return thunkAPI.fulfillWithValue(data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -16,9 +15,10 @@ export const __addWriteThunk = createAsyncThunk(
 );
 
 export const __getPostThunk = createAsyncThunk(
-  "GET_TODO",
+  "GET_POST",
   async (payload, thunkAPI) => {
     try {
+      console.log(data)
       const { data } = await axios.get(`http://localhost:3001/posts`, payload);
       return thunkAPI.fulfillWithValue(data);
     } catch (error) {
@@ -27,26 +27,27 @@ export const __getPostThunk = createAsyncThunk(
   }
 );
 
-// export const __updateTodoThunk = createAsyncThunk(
-//   'UPDATE_TODO',
-//   async (arg, thunkAPI) => {
-//     try {
-//       axios.patch(`http://localhost:3001/todos/${arg.id}`, arg);
-//       return thunkAPI.fulfillWithValue(arg);
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.code);
-//     }
-//   }
-// );
+export const __updatePostThunk = createAsyncThunk(
+  'UPDATE_POST',
+  async (id, thunkAPI) => {
+    try {
+      axios.patch(`http://localhost:3001/todos/${id}`, id);
+      return thunkAPI.fulfillWithValue(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.code);
+    }
+  }
+);
 
 const initialState = {
   posts: [],
   error: null, // 서버랑 통신 실패 시 나타내는 에러메세지 담아놓는 값
   isLoading: false, // 서버에서 posts를 가져오는 상태값
+  detailPosts: null
 };
 
 export const addupdateSlice = createSlice({
-  name: "posts",
+  name: 'posts',
   initialState,
   reducers: {
     addPost: (state, action) => {
@@ -67,7 +68,7 @@ export const addupdateSlice = createSlice({
     },
     [__getPostThunk.fulfilled]: (state, action) => {
       state.isLoading = false;
-      state.posts = action.payload;
+      state.detailPosts = action.payload;
     },
     [__getPostThunk.rejected]: (state, action) => {
       state.isLoading = false;
@@ -76,17 +77,17 @@ export const addupdateSlice = createSlice({
     [__getPostThunk.pending]: (state) => {
       state.isLoading = true;
     },
-    // [__updateTodoThunk.fulfilled]: (state, action) => {
-    //   state.isLoading = false;
-    //   state.todo = action.payload;
-    // },
-    // [__updateTodoThunk.pending]: (state) => {
-    //   state.isLoading = true;
-    // },
-    // [__updateTodoThunk.rejected]: (state, action) => {
-    //   state.isLoading = false;
-    //   state.error = action.payload;
-    // },
+    [__updatePostThunk.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.todo = action.payload;
+    },
+    [__updatePostThunk.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__updatePostThunk.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
   },
 });
 
